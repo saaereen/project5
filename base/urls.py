74ed4aab2d6f0a7a100cbbb6198788b1c727f5d7
Shyryn <shyryn1806@gmail.com>
2024-04-26 22:01:50 +0500

    user registration

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    path('register/', views.registerPage, name="register"),
+
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
 
--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -16,13 +17,33 @@ from .forms import RoomForm
 
 ]
 '''
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Something went wrong")
+
+
+    return render(request, 'base/login_register.html', {'form':form} )
+
+
+
 def loginPage(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('home')
 
     if request.method == 'POST': 
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try :
             user = User.objects.get(username=username)
@@ -36,7 +57,7 @@ def loginPage(request):
         else:
             messages.error(request, 'Username or password does not exist ')
 
-    context = {}
+    context = {'page':page}
     return render(request, 'base/login_register.html', context)
 
 
@@ -45,7 +66,7 @@ def logoutUser(request):
     logout(request)
     return(redirect('home'))
 
-
+    
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') !=None else ''
